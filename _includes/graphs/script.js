3721d8b008d8b44181ee9e4d810fe9c906d7e08b
Lapalap <kirill079@gmail.com>
2017-09-29 16:11:20 +0300

    Validation

--- _includes/graphs/script.js
+++
@@ -1,8 +0,0 @@
-<script>
-    function PlaySound(melody) {
-        alert("On Press of "+melody);
-        var path = "path\\to\\melody\\"
-        var snd = new Audio(path + melody + ".mp3");
-        snd.play();
-    }
-</script>
